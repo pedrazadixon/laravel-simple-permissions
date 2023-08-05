8f354d92cc753b0af41527591e9bb1bad2ebd67a
Dixon Pedraza <pedrazadixon@gmail.com>
2023-08-05 16:26:25 -0500

    fix get roles in auth user

--- src/Middleware/Permissions.php
+++ src/Middleware/Permissions.php
@@ -5,17 +5,21 @@ namespace Pedrazadixon\LaravelSimplePermissions\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Pedrazadixon\LaravelSimplePermissions\Models\User;
 
 class Permissions
 {
     public function handle(Request $request, Closure $next): Response
     {
+        // find authenticated user from custom user model
+        $authenticated_user = User::find(auth()->user()->id);
+
         // allow admin to do anything
-        if (auth()->user()->role_id == 1)
+        if ($authenticated_user->role_id == 1)
             return $next($request);
 
         // validate permissions for current user
-        $user_permissions = auth()->user()->rol->permissions->pluck('action')->toArray();
+        $user_permissions = $authenticated_user->rol->permissions->pluck('action')->toArray();
 
         if (str_contains($request->route()->getActionName(), 'Controller@')) {
             $requested_action = $request->route()->getAction()['controller'];
